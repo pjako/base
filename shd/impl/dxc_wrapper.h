8d11efb5af045af02a1e412a5ed1908ff7ed812f
Peter Jakobs <p4jako@googlemail.com>
2024-01-10 13:51:31 +0100

    Updating shd to support legacy Res binding

--- shd/impl/dxc_wrapper.h
+++ shd/impl/dxc_wrapper.h
@@ -1,32 +1,73 @@
 #ifndef DXC_WRAPPER_H
 #define DXC_WRAPPER_H
 
-typedef struct dxc_Dll {
-    os_Dl* dxcDll;
-    void* createProc;
-} dxc_Dll;
-
-typedef struct dxc_CompileResult {
-    Str8 spvCode;
-    Str8 diagMessage;
-} dxc_CompileResult;
-
-bool dxc_loadLib(dxc_Dll* dll);
-
-typedef struct dxc_Instance dxc_Instance;
-
 typedef enum dxc_shaderType {
     dxc_shaderType_vertex,
     dxc_shaderType_pixel,
     dxc_shaderType_compute,
 } dxc_shaderType;
 
+typedef enum dxc_binaryShader {
+    dxc_binaryShader_spirv = 0,
+    dxc_binaryShader_dxil
+} dxc_binaryShader;
+
 typedef struct StrPair {
     Str8 left;
     Str8 right;
 } StrPair;
 
-dxc_Instance* dxc_create(Arena* arena, dxc_Dll* dll);
-dxc_CompileResult dxc_compileHlslToSpv(Arena* arena, dxc_Instance* dxcInstance, dxc_shaderType type, Str8 source, Str8 inputFileName, Str8 entryPoint, StrPair* defines, u32 defineCount, Str8* params, u32 paramsCount);
+typedef struct DxCompiler DxCompiler;
+
+API DxCompiler* dxc_create(Arena* arena);
+
+typedef struct dxc_CompileOptions {
+    struct {
+        u32 minor;
+        u32 major;
+    } shaderModel;
+    dxc_binaryShader binaryShaderType;
+    u32 packMatricesInRowMajor;
+    bx  enable16bitTypes;
+    bx  enableDebugInfo;
+    u32 disableOptimizations;
+    u32 optimizationLevel;
+    u32 shiftAllCBuffersBindings;
+    u32 shiftAllUABuffersBindings;
+    u32 shiftAllSamplersBindings;
+    u32 shiftAllTexturesBindings;
+    //dxil only
+    bx asModule;
+} dxc_CompileOptions;
+
+typedef struct dxc_CompileDesc {
+    DxCompiler* dxCompiler;
+    dxc_shaderType type;
+    Str8 source;
+    Str8 inputFileName;
+    Str8 entryPoint;
+    StrPair* defines;
+    u32 defineCount;
+    Str8 (*includeLoadCallback)(Str8 includeName, void* userPtr);
+    void* userPtr;
+    dxc_CompileOptions options;
+} dxc_CompileDesc;
+
+typedef struct dxc_ReflectionResultDesc {
+    Str8 descs; // The underneath type is ReflectionDesc
+    uint32_t descCount = 0;
+    uint32_t instructionCount = 0;
+} dxc_ReflectionResultDesc;
+
+typedef struct dxc_CompileResult {
+    Str8 target;
+
+    Str8 errorWarningMsg;
+    bool hasError;
+
+    dxc_ReflectionResultDesc reflection;
+} dxc_CompileResult;
+
+dxc_CompileResult dxc_compileHlslToSpv(Arena* arena, dxc_CompileDesc* desc);
 
 #endif
--- tools_src/shdc.c
+++ tools_src/shdc.c
@@ -3,6 +3,7 @@
 #include "base/base_mem.h"
 #include "base/base_str.h"
 #include "base/base_args.h"
+#include "os/os.h"
 
 #include "shd/shd.h"
 
@@ -23,11 +24,28 @@ i32 main(i32 argc, char* argv[]) {
     argc = debugArgc;
 #endif
 
-
+#if 0
     arg_Opt options[] = {
         {str8("shader"), 's', arg_optType_string, arg_flag_required | arg_flag_requiredValue, STR_EMPTY, str8("SHD shader file path."), str8("Absolute path to the shader file.")},
         {str8("header"), 'h', arg_optType_string, arg_flag_none, STR_EMPTY, str8("generated header output file path."), str8("Absolute path where the generated header should be created.")},
         {str8("prefix"), 'p', arg_optType_string, arg_flag_none, str_lit("shd_"), str8("prefix used in generated header"), str8("Add user defined prefix to variables and functions in the generated header.\nThe default shader is \"shd_\"")},
     };
+#endif
 
+
+
+    BaseMemory baseMem = os_getBaseMemory();
+    Arena* mainArena = mem_makeArena(&baseMem, MEGABYTE(8));
+
+    Str8 targetPath = s8("/Users/peterjakobs/pjako/base_kit/_examples/sample_texture.hlsl");
+
+    Str8 fileContent = os_fileRead(mainArena, targetPath);
+
+    Str8 headerFileContent = shd_generateShader(mainArena, targetPath, fileContent, shd_variation_ogl40);
+
+    Str8 headerPath = s8("/Users/peterjakobs/pjako/base_kit/_examples/sample_texture.hlsl.h");
+
+    os_writeFile(headerPath, headerFileContent);
+
+    return 0;
 }
